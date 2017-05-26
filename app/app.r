b67c06fc81efab6eda78b05313400a26fb1f48af
Metod Jazbec <jazbec.metod@gmail.com>
2017-05-26 09:20:33 +0200

    minor

--- app/app.r
+++ app/app.r
@@ -1,25 +1,30 @@
 library(shiny)
 #deployApp()
 ui <- fluidPage(
-  headerPanel('Daily news archive (2008-2016)'),
-  dateInput(inputId = "datum",
-              label = "Date [YYYY-MM-DD]",
-            min = as.Date(0, origin = "2008-06-08"),
-            max = as.Date(2945, origin = "2008-06-08")
-            ),
-  numericInput(inputId = "stevilo",
-               label = "Number of news [1-25]",
-               value = 1,
-               min = 1,
-               max = 20
-               ),
-  submitButton("Display"),
+  wellPanel(
+    fluidRow(
+      headerPanel('Daily news archive (2008-2016)'),
+      column(3, dateInput(inputId = "datum",
+                          label = "Date [YYYY-MM-DD]",
+                          min = as.Date(0, origin = "2008-06-08"),
+                          max = as.Date(2945, origin = "2008-06-08")
+      ),
+      numericInput(inputId = "stevilo",
+                   label = "Number of news [1-25]",
+                   value = 1,
+                   min = 1,
+                   max = 20
+      ),
+      actionButton("display", "Display")),
+      column(5, tags$img(height = 100, width = 200, 
+                         src = "reddit.png"), offset = 4))),
   tableOutput("tabelaNovic")
 )
 
-server <- function(input, output) {
 
-  output$tabelaNovic <- renderTable({
+server <- function(input, output) {
+  
+  novice <- eventReactive(input$display, {
     novice <- read.csv("RedditNews.csv")
     novice$Date <- as.Date(novice$Date)
     novice$News <- as.character(novice$News)
@@ -27,7 +32,10 @@ server <- function(input, output) {
     novice <- novice[1:input$stevilo,]
     novice$nr <- c(1:input$stevilo)
     novice <- novice[,c("nr", "News")]
-    novice
+  })
+  
+  output$tabelaNovic <- renderTable({
+    novice()
   })
 }
 
