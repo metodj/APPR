58687accbdfc4cf45db68eeeee2b274c978a170c
Metod Jazbec <jazbec.metod@gmail.com>
2017-05-28 21:35:21 +0200

    shiny ...

--- app/app.r
+++ app/app.r
@@ -1,24 +1,41 @@
 library(shiny)
+library(shinythemes)
 #deployApp()
 ui <- fluidPage(
-  wellPanel(
-    fluidRow(
-      headerPanel('Daily news archive (2008-2016)'),
-      column(3, dateInput(inputId = "datum",
-                          label = "Date [YYYY-MM-DD]",
-                          min = as.Date(0, origin = "2008-06-08"),
-                          max = as.Date(2945, origin = "2008-06-08")
-      ),
-      numericInput(inputId = "stevilo",
-                   label = "Number of news [1-25]",
-                   value = 1,
-                   min = 1,
-                   max = 20
-      ),
-      actionButton("display", "Display")),
-      column(5, tags$img(height = 100, width = 200, 
-                         src = "reddit.png"), offset = 4))),
-  tableOutput("tabelaNovic")
+  theme = shinytheme("united"),
+  tabsetPanel(
+    tabPanel(title = "Title search",
+             wellPanel(
+               fluidRow(
+                 headerPanel("Daily news' titles archive (2008-2016)"),
+                 column(3, dateInput(inputId = "datum",
+                                     label = "Date [YYYY-MM-DD]",
+                                     min = as.Date(0, origin = "2008-06-08"),
+                                     max = as.Date(2945, origin = "2008-06-08")
+                 ),
+                 numericInput(inputId = "stevilo",
+                              label = "Number of news [1-25]",
+                              value = 1,
+                              min = 1,
+                              max = 20
+                 ),
+                 actionButton("display", "Display")),
+                 column(5, tags$img(height = 100, width = 200, 
+                                    src = "reddit.png"), offset = 4))),
+             tableOutput("tabelaNovic")),
+    tabPanel(title = "Word frequency",
+             wellPanel(
+               fluidRow(
+                 headerPanel("Word frequency"),
+                 column(3, 
+                        textInput(inputId = "beseda",
+                                  label = "Insert word"
+                        ),
+                        actionButton("display2", "Display")),
+                 column(5, tags$img(height = 150, width = 200, 
+                                    src = "wordcloud.png"), offset = 4))),
+             tableOutput("pogostostBesede")), type = "pills"
+  )
 )
 
 
@@ -34,9 +51,20 @@ server <- function(input, output) {
     novice <- novice[,c("nr", "News")]
   })
   
+  word <- eventReactive(input$display2, {
+    word <- read.csv("besede.csv")
+    word <- word[word$Beseda == input$beseda,]
+    word <- word[,2:3]
+  })
+  
   output$tabelaNovic <- renderTable({
     novice()
   })
+  
+  output$pogostostBesede <- renderTable({
+    word()
+  }, bordered = TRUE)
+  
 }
 
 
